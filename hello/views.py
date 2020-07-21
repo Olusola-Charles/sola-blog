445cfdd6fe348c6c522aeeb6015cdc77ef758fdd
Olusola Ayodele <solamoal@gmail.com>
2020-07-21 16:44:51 +0100

    Demo

--- hello/views.py
+++ hello/views.py
@@ -7,7 +7,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    times = int(os.environ.get('TIMES', 3)
+    times = int(os.environ.get('TIMES', 3))
     return HttpResponse('Hello!' * times)
 
 
