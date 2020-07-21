3bb55c739bc79db194da7cf5bd0dbdde7b598f54
Olusola Ayodele <solamoal@gmail.com>
2020-07-21 14:51:16 +0100

    Demo

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,7 @@ import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from models import Greeting
 
 # Create your views here.
 def index(request):
