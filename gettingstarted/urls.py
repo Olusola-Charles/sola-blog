22b60a215fe02c63834eb792443ad63953729c2d
Olusola Ayodele <solamoal@gmail.com>
2020-07-21 09:12:54 +0100

    Demo

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -16,6 +16,6 @@ import hello.views
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
+    path("db/", hello.views, name="db"),
     path("admin/", admin.site.urls),
 ]
